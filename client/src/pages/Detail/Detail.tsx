3a36d714a72d3106a2aaa94c62d35634d391879c
Seokjun7074 <sukjune96@gmail.com>
2023-10-04 07:03:28 +0000

    :sparkles: feat: 사진으로 저장하기 기능 구현

--- client/src/pages/Detail/Detail.tsx
+++ client/src/pages/Detail/Detail.tsx
@@ -3,14 +3,33 @@ import * as S from '@/pages/Detail/Detsil.style';
 import { useGetTransactionDetail } from '@/hooks/apiHooks/useGetTransactionDetail';
 import { filteredImage } from '@/utils/filterImage';
 import { setMoneyRegex } from '@/utils/regex';
+import { useRef } from "react";
+import { toPng } from "html-to-image";
 
 const Detail = () => {
   const { transactionId } = useParams();
   const { transactionDetailData } = useGetTransactionDetail(Number(transactionId));
 
+  const ref = useRef<HTMLDivElement>(null);
+  const htmlToImageConvert = () => {
+    if (ref.current === null) {
+      return
+    }
+    toPng(ref.current, { cacheBust: false })
+      .then((dataUrl) => {
+        const link = document.createElement("a");
+        link.download = String(Date.now());
+        link.href = dataUrl;
+        link.click();
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
   return (
     <S.DetailWrapper>
-      <S.ReceiptWrapper>
+      <S.ReceiptWrapper ref={ref}>
         <S.TransactionTitle>{transactionDetailData.title}</S.TransactionTitle>
         <S.EmotionImageContainer>{filteredImage(transactionDetailData.emoticon)}</S.EmotionImageContainer>
         <S.Money>{setMoneyRegex(String(transactionDetailData.amount))}</S.Money>
@@ -26,7 +45,7 @@ const Detail = () => {
           <S.EmotionInfo>{transactionDetailData.accountName}</S.EmotionInfo>
         </S.EmotionInfoWrapper>
       </S.ReceiptWrapper>
-      <S.SavePhotoButton>사진으로 저장하기</S.SavePhotoButton>
+      <S.SavePhotoButton onClick={() => htmlToImageConvert()}>사진으로 저장하기</S.SavePhotoButton>
     </S.DetailWrapper>
   );
 };
