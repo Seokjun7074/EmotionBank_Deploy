891a984b7914a7f057372842b9b1e604b9766b39
Seokjun7074 <sukjune96@gmail.com>
2023-10-05 00:44:45 +0000

    :bug: fix: accessToken 재발급 에러 수정

--- client/src/apis/index.ts
+++ client/src/apis/index.ts
@@ -38,38 +38,11 @@ axiosInstance.interceptors.response.use(
     } = error;
     const originalRequest = config;
     if (data.errorCode === 'J-003') {
-      if (!isRefreshing) {
-        isRefreshing = true;
-        try {
-          const response = await renewAccessToken();
-          // console.log(response);
-          localStorage.setItem('accessToken', response.accessToken);
-          isRefreshing = false;
-          // 다시 시도하지 못한 요청 처리
-          failedRequests.forEach(cb => cb(response.accessToken));
-          failedRequests = [];
-          return axiosInstance(originalRequest);
-        } catch (error) {
-          console.log('토큰만료시 재요청 보냈을 때의 ' + error);
-        }
-      } else {
-        return new Promise((resolve, reject) => {
-          failedRequests.push(newAccessToken => {
-            originalRequest.headers['Authorization'] = `Bearer ${newAccessToken}`;
-            resolve(axiosInstance(originalRequest));
-          });
-        });
-      }
-    } else if (data.errorCode === 'J-002') {
-      const navigate = useNavigate();
-      const goToLogin = () => navigate('/login');
-      console.log('유효하지 않은 refresh token');
-      alert('로그인 화면으로 이동합니다.');
-      goToLogin();
+      const {accessToken} = await renewAccessToken()
+      localStorage.setItem('accessToken', accessToken)
+      originalRequest.headers.Authorization = `Bearer ${accessToken}`
+      return axiosInstance(originalRequest)
     }
-    // } else if (status === 500) {
-    //   console.log('여긴 500 에러', error);
-    // }
     return Promise.reject(error);
   },
 );
